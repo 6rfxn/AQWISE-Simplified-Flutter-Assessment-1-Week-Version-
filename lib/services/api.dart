4edb07afb0e8a1dd462c516d11288b9016c31b4b
irfan <earfxn@gmail.com>
2025-12-30 19:35:07 +0800

    Initial commit

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,18 +1,33 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:assignment/Models/Post_Model.dart';
+import 'dart:convert'; // JSON
+import 'package:http/http.dart' as http; // requests
+import 'package:assignment/Models/post_model.dart';
 
 class ApiService {
-  static const String _url = 'https://jsonplaceholder.typicode.com/posts';
+  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
 
-  Future<List<Post>> get() async {
-    final response = await http.get(Uri.parse(_url));
-    
+  // 1) Go URL
+  // 2) Get JSON
+  // 3) JSON > List<Post>
+
+  // To Remember
+  // throw - error(), print - print()
+  // Response Status Code https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
+
+  Future<List<Post>> fetchPosts() async {
+    final url = Uri.parse('$baseUrl/posts');
+    final response = await http.get(url);
+
     if (response.statusCode != 200) {
-      throw Exception('Failed to load posts');
+      throw Exception('Err (code: ${response.statusCode})');
     }
 
-    final data = jsonDecode(response.body) as List;
-    return data.map((json) => Post.fromJson(json as Map<String, dynamic>)).toList();
+    final List<dynamic> decodedJson =
+        jsonDecode(response.body) as List<dynamic>;
+
+    final List<Post> posts = decodedJson
+        .map((item) => Post.fromJson(item as Map<String, dynamic>))
+        .toList();
+
+    return posts;
   }
 }
